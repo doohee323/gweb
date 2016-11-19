6f070f9c999b4692788f79b293b1cee19aa67cff
Dewey Hong <dhong@fortinet.com>
2016-11-19 15:02:07 -0800

    .

--- gweb-sb/src/build_deb.sh
+++ gweb-sb/src/build_deb.sh
@@ -1,3 +1,8 @@
+cp -rf target/gweb-sb-1.0.0-jar-with-dependencies.jar src/gweb-sb/usr/local/etc/gweb-sb
+cp -rf src/main/resources/*.properties src/gweb-sb/usr/local/etc/gweb-sb
+cp -rf src/main/resources/*.conf src/gweb-sb/usr/local/etc/gweb-sb
+cp -rf src/main/resources/*.xml src/gweb-sb/usr/local/etc/gweb-sb
+
 chmod 775 gweb-sb/DEBIAN/postinst
 
 dpkg -b gweb-sb
