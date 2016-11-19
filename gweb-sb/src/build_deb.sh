e9f93b07a819851a4bcf34b0c6be09d42542d58f
Dewey Hong <dhong@fortinet.com>
2016-11-19 15:15:49 -0800

    pkg-build

--- gweb-sb/src/build_deb.sh
+++ gweb-sb/src/build_deb.sh
@@ -1,13 +1,14 @@
 cd ..
 mvn clean compile package
 
-mkdir -p gweb-sb/usr/local/etc/gweb-sb
+mkdir -p src/pkg-build/usr/local/etc/gweb-sb
 
-cp -rf target/gweb-sb-1.0.0-jar-with-dependencies.jar src/gweb-sb/usr/local/etc/gweb-sb
-cp -rf main/resources/*.properties gweb-sb/usr/local/etc/gweb-sb
-cp -rf main/resources/*.conf gweb-sb/usr/local/etc/gweb-sb
-cp -rf main/resources/*.xml gweb-sb/usr/local/etc/gweb-sb
+cp -rf target/gweb-sb-1.0.0-jar-with-dependencies.jar src/pkg-build/usr/local/etc/gweb-sb
+cp -rf src/main/resources/*.properties src/pkg-build/usr/local/etc/gweb-sb
+cp -rf src/main/resources/*.conf src/pkg-build/usr/local/etc/gweb-sb
+cp -rf src/main/resources/*.xml src/pkg-build/usr/local/etc/gweb-sb
 
-chmod 775 gweb-sb/DEBIAN/postinst
+chmod 775 src/pkg-build/DEBIAN/postinst
 
-dpkg -b gweb-sb
+dpkg -b pkg-build
+mv pkg-build.deb gweb-sb.deb
