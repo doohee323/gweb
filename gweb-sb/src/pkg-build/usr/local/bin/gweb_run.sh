f170850de54edce0acfedac5426209c7c1ad64c6
Dewey Hong <dhong@fortinet.com>
2016-11-19 20:30:29 -0800

    .

--- gweb-sb/src/pkg-build/usr/local/bin/gweb_run.sh
+++ gweb-sb/src/pkg-build/usr/local/bin/gweb_run.sh
@@ -1,5 +1,5 @@
 cd /usr/local/etc/gweb-sb
 
-java -jar /usr/local/etc/gweb-sb/gweb-sb-1.0.0.jar --spring.profiles.active=local
+#java -jar /usr/local/etc/gweb-sb/gweb-sb-1.0.0.jar --spring.profiles.active=local
 
 #mvn spring-boot:run
